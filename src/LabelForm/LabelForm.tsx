876ac7438c9920760bedd37243494074f70768b7
Dafi <dafinmaulana18@gmail.com>
2025-11-07 21:36:15 +0700

    feat: add color props

--- src/LabelForm/LabelForm.tsx
+++ src/LabelForm/LabelForm.tsx
@@ -5,12 +5,17 @@ import Color from '../Color/Color';
 interface Props {
   title: string;
   required?: boolean;
+  color?: string;
 }
 
-export default function LabelForm({ title, required }: Props) {
+export default function LabelForm({
+  title,
+  required,
+  color = Color.gray[900],
+}: Props) {
   return (
     <View>
-      <Typography variant="t2" weight="semibold">
+      <Typography variant="t2" weight="semibold" color={color}>
         {title}{' '}
         {required && (
           <Typography variant="t2" weight="semibold" color={Color.danger[500]}>
