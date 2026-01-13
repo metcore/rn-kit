1b348300adb772cf450854ab17a7f3d18cc0d285
Dafi <dafinmaulana18@gmail.com>
2026-01-13 16:30:54 +0700

    feat: add props fill screen and improve image position

--- src/Provider/RequiredUpdateAppsProvider.tsx
+++ src/Provider/RequiredUpdateAppsProvider.tsx
@@ -1,64 +1,81 @@
-import { Image, StyleSheet, View } from 'react-native';
-import { Button, Color, Container, Modal, Typography } from '..';
-import type { RequiredUpdateAppsProviderProps } from './type';
 import { useEffect } from 'react';
+import { Image, Platform, StyleSheet, View } from 'react-native';
+import { Button, Card, Color, Modal, Typography } from '..';
+import type { RequiredUpdateAppsProviderProps } from './type';
 
 export default function RequiredUpdateAppsProvider({
   title,
   description,
   buttonText = 'Update Sekarang',
   onUpdate,
+  fillScreen = false,
 }: RequiredUpdateAppsProviderProps) {
   useEffect(() => {
     onUpdate?.(false);
   }, [onUpdate]);
   return (
-    <View>
-      <Modal height={300} isOpen={true} closable={false}>
+    <Modal
+      isOpen={true}
+      closable={false}
+      modalStyle={fillScreen ? styles.modalFill : undefined}
+    >
+      <View style={{ alignItems: 'center' }}>
         <Image
           source={require('../_assets/updateapps.png')}
           style={styles.image}
         />
-        <Container style={styles.container}>
+      </View>
+      <Card style={styles.container} borderColor="transparent">
+        <View style={styles.content}>
           <Typography
-            variant="h4"
+            variant="p1"
             weight="semibold"
-            color={Color.gray[800]}
+            color={Color.gray[900]}
             center
           >
             {title}
           </Typography>
-          <Typography
-            variant="t1"
-            weight="regular"
-            color={Color.gray[600]}
-            center
-          >
+          <Typography variant="t2" color={Color.gray[700]} center>
             {description}
           </Typography>
-          <View style={{}}>
-            <Button
-              title={buttonText}
-              color="primary"
-              onPress={() => {
-                onUpdate?.(false);
-              }}
-            />
-          </View>
-        </Container>
-      </Modal>
-    </View>
+        </View>
+
+        <View>
+          <Button
+            title={buttonText}
+            color="primary"
+            onPress={() => {
+              onUpdate?.(false);
+            }}
+          />
+        </View>
+      </Card>
+    </Modal>
   );
 }
 
 const styles = StyleSheet.create({
   image: {
-    position: 'absolute',
-    marginTop: -60,
-    left: 30,
+    marginTop: Platform.OS === 'ios' ? -60 : -75,
+    objectFit: 'contain',
+    height: 124,
   },
   container: {
     gap: 20,
-    marginTop: 80,
+    padding: Platform.OS === 'ios' ? 24 : 10,
+  },
+  content: {
+    gap: 10,
+  },
+  modalFill: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    width: '100%',
+    height: '100%',
+    maxHeight: 'auto',
+    justifyContent: 'center',
   },
 });
